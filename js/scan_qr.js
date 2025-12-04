4ad0115c2ee246f731f1804cde48c2bcd4eb05b5
blankaex <blankaex@protonmail.com>
2025-12-04 17:58:11 +0900

    implemented status display

--- js/scan_qr.js
+++ js/scan_qr.js
@@ -2,13 +2,14 @@
 let akey = '';
 let skey = '';
 let scd  = '';
-let html5QrCode = null;
+let reader = null;
+let connected = false;
 
 function scan_qr()
 {
-    if (!html5QrCode) html5QrCode = new Html5Qrcode("reader");
+    if (!reader) reader = new Html5Qrcode("reader");
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-    html5QrCode
+    reader
         // attempt to use phone back camera
         .start({ facingMode: { exact: "environment"} }, config, scan_success)
         // otherwise enumerate devices and prompt user to select
@@ -33,8 +34,8 @@ function scan_qr()
 
 async function stop_scanning()
 {
-    if(html5QrCode && html5QrCode.getState() == Html5QrcodeScannerState.SCANNING) {
-        await html5QrCode.stop();
+    if(reader && reader.getState() == Html5QrcodeScannerState.SCANNING) {
+        await reader.stop();
     }
 }
 
@@ -45,7 +46,7 @@ async function set_device(devices, config, scan_success)
     // find matching camera device
     const dev = devices.find(x => x.label === $("#select0").val());
     // run new scanner with selected camera device
-    if (dev) await html5QrCode.start({ deviceId: { exact: dev.id } }, config, scan_success);
+    if (dev) await reader.start({ deviceId: { exact: dev.id } }, config, scan_success);
 }
 
 async function scan_success(decodedText, decodedResult)
@@ -55,5 +56,14 @@ async function scan_success(decodedText, decodedResult)
     // rudimentary error checking
     if(!/rdn_[A-Za-z0-9]+\.[A-Za-z0-9]+,[A-Za-z0-9]+,[0-9]+/.test(decodedText)) throw new Error("Invalid QR Code");
     [akey, skey, scd] = decodedText.split('/')[2].split(',');
+    update_status();
     console.log("akey: " + akey + "\nskey: " + skey + "\nscd: " + scd);
+}
+
+function update_status()
+{
+    $('.widget').removeClass('red-bg');
+    $('.widget').addClass('navy-bg');
+    $('#connected').text("Connected");
+    $('#keys').html(`<br/>akey: ${akey}<br/>skey: ${skey}<br/>scd: ${scd}`, akey, skey, scd);
 }

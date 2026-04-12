d09da61425f64b7f51cfdee9858e759e9b91c70c
blankaex <blankaex@protonmail.com>
2026-04-13 01:05:24 +0900

    temporary patch to mitigate #16 until response is provided

--- js/song.js
+++ js/song.js
@@ -129,13 +129,15 @@ function append_history(song_code) {
 // appends a single song row to a given table body element
 function append_table(table_body, song_code, last_played = null) {
     const song_cache = JSON.parse(localStorage.getItem("song_cache"));
-    const row = $(`<tr id=${song_code} onclick="fill_song_modal(this)">`);
-    row.append($("<td>").text(normalize_song(song_code)));
-    row.append($("<td>").text(song_cache[song_code].artist));
-    if (table_body == "#history-table-body") {
-        row.append($("<td>").text(last_played));
+    if (song_code in song_cache) {
+        const row = $(`<tr id=${song_code} onclick="fill_song_modal(this)">`);
+        row.append($("<td>").text(normalize_song(song_code)));
+        row.append($("<td>").text(song_cache[song_code].artist));
+        if (table_body == "#history-table-body") {
+            row.append($("<td>").text(last_played));
+        }
+        $(table_body).append(row);
     }
-    $(table_body).append(row);
 }
 
 // shows the song detail modal and populates it with data for the selected song
